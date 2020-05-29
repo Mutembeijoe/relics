7ab7d6dfbfb0e6126c6955473b34cf172c6b0a72
mutembeijoe <mutembeijoe@gmail.com>
2020-05-29 20:56:55 +0300

    makes the whole display card a clickable link

--- components/product-card-display/product-card-display.component.jsx
+++ components/product-card-display/product-card-display.component.jsx
@@ -3,26 +3,27 @@ import Card from "react-bootstrap/Card";
 import productCardStyles from "./product-card-display.module.scss";
 
 const ProductCardDisplay = ({ item }) => (
-  <Card
-    className={`${productCardStyles.card} shadow mx-3 mb-5 bg-white rounded`}
-  >
-    <div className={`${productCardStyles.cardImgBox}`}>
-      <img
-        src={item.image_url}
-        className={`${productCardStyles.cardImg}`}
-        alt={item.product_name}
-      />
-    </div>
-
-    <Card.Body className={`${productCardStyles.cardBody}`}>
-      <Card.Title className="text-primary">{item.product_name}</Card.Title>
-      <div className={productCardStyles.cardFooter}>
-        <div className={productCardStyles.price}>
-          <span className="text-muted">KSh</span>
-          <span className="mx-1 text-primary">{item.price}</span>
+  <Link href="/product/[slug]" as={`/product/${item.product_slug}`}>
+    <a>
+      <Card
+        className={`${productCardStyles.card} shadow mx-3 mb-5 bg-white rounded`}
+      >
+        <div className={`${productCardStyles.cardImgBox}`}>
+          <img
+            src={item.image_url}
+            className={`${productCardStyles.cardImg}`}
+            alt={item.product_name}
+          />
         </div>
-        <Link href="/product/[slug]" as={`/product/${item.product_slug}`}>
-          <a>
+
+        <Card.Body className={`${productCardStyles.cardBody}`}>
+          <Card.Title className="text-primary">{item.product_name}</Card.Title>
+          <div className={productCardStyles.cardFooter}>
+            <div className={productCardStyles.price}>
+              <span className="text-muted">KSh</span>
+              <span className="mx-1 text-primary">{item.price}</span>
+            </div>
+
             <div className={productCardStyles.viewProductDetail}>
               <div className="px-2">
                 view details
@@ -34,11 +35,11 @@ const ProductCardDisplay = ({ item }) => (
             >
               <i className="mdi mdi-cart mdi-24px"></i>
             </div>
-          </a>
-        </Link>
-      </div>
-    </Card.Body>
-  </Card>
+          </div>
+        </Card.Body>
+      </Card>
+    </a>
+  </Link>
 );
 
 export default ProductCardDisplay;
