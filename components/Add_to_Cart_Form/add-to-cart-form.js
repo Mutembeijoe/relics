cc1654cecf2268bd5c3c811c082db4e88ae5aacc
mutembeijoe <mutembeijoe@gmail.com>
2020-05-29 20:57:18 +0300

    minor fixes

--- components/Add_to_Cart_Form/add-to-cart-form.js
+++ components/Add_to_Cart_Form/add-to-cart-form.js
@@ -91,7 +91,7 @@ const AddToCart = ({ product, addToCart }) =>  {
                 <option value="">Select Size</option>
                 {options.map((option) => (
                   <option value={option} key={option}>
-                    {option}
+                    {option.toUpperCase()}
                   </option>
                 ))}
               </Form.Control>
--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -2,27 +2,33 @@ import Head from "next/head";
 import Header from "../Header/header";
 import styles from "./layout.module.scss";
 import { useState } from "react";
-import cn from 'classnames'
+import cn from "classnames";
 import Cart from "../Cart/cart.component";
 
 export default function Layout({ children }) {
-  const [cartOpen, toggleCartOpen] = useState(false)
+  const [cartOpen, toggleCartOpen] = useState(false);
 
   return (
     <div>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={cn(styles.container, {[styles.containerCollapsed]:cartOpen})}>
-        <div className={styles.main}>
-          <Header toggleCartOpen={toggleCartOpen} cartOpen={cartOpen}></Header>
-          <main className="pt-5">{children}</main>
-        </div>
-        <footer>
-          <p className="text-primary">Ndirangujoe &copy;{new Date().getFullYear()}</p>
-        </footer>
+      <div
+        className={cn(styles.container, {
+          [styles.containerCollapsed]: cartOpen,
+        })}
+      >
+        <Header toggleCartOpen={toggleCartOpen} cartOpen={cartOpen} />
+          <div className={styles.main}>
+            <main className="pt-5">{children}</main>
+          </div>
+          <footer>
+            <p className="text-primary">
+              Ndirangujoe &copy;{new Date().getFullYear()}
+            </p>
+          </footer>
       </div>
-      <Cart cartOpen={cartOpen} toggleCartOpen={toggleCartOpen}/>
+      <Cart cartOpen={cartOpen} toggleCartOpen={toggleCartOpen} />
     </div>
   );
 }
