42862c110aa1f6dfe4a6f3a45d740e5c461bdc57
mutembeijoe <mutembeijoe@gmail.com>
2020-06-01 12:28:17 +0300

    adds knexfile for knexjs environmental configurations

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+require("dotenv").config({ path: "./.env.local" });
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_LOCAL_HOST,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_NAME,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    debug: true,
+  },
+};
