0654b29a64d00d447b1524b6c84cc04b1a51af54
mutembeijoe <mutembeijoe@gmail.com>
2020-06-02 12:27:35 +0300

    adds formik validation on checkout page

--- components/checkout_form/checkout-form.component.jsx
+++ components/checkout_form/checkout-form.component.jsx
@@ -2,6 +2,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import * as yup from "yup";
+import { Formik } from "formik";
 
 const CheckoutForm = () => {
   const schema = yup.object({
@@ -23,119 +24,191 @@ const CheckoutForm = () => {
       .string()
       .oneOf(["Nairobi", "Kajiado", "Kiambu", "Mombasa", "Nakuru", "Nyeri"])
       .required("You must select a county"),
-    terms: yup.boolean().equals(true).required(),
+    terms: yup.boolean().required().equals([true])
   });
   return (
-    <Form>
-      {/* Contacts Section */}
-      <fieldset className="my-3">
-        <legend>Contact Information</legend>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmailAddress">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              name="email"
-              type="email"
-              placeholder="Email Address"
-              className="rounded"
-            />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridPhone">
-            <Form.Label>Phone</Form.Label>
-            <Form.Control
-              name="phone"
-              placeholder="+254 712..."
-              className="rounded"
-            />
-          </Form.Group>
-        </Form.Row>
-      </fieldset>
+    <Formik
+      validationSchema={schema}
+      onSubmit={(value, actions) => {
+        console.log(value);
+        actions.resetForm();
+        actions.setSubmitting(false)
+      }}
+      initialValues={{
+        email: "",
+        phone: "",
+        firstName: "",
+        lastName: "",
+        address: "",
+        address2: "",
+        town: "",
+        county: "",
+        terms: false,
+      }}
+    >
+      {({
+        handleSubmit,
+        values,
+        handleChange,
+        touched,
+        isValid,
+        errors,
+        isSubmitting,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+        {console.log(errors)}
+          {/* Contacts Section */}
+          <fieldset className="my-3">
+            <legend>Contact Information</legend>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmailAddress">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  name="email"
+                  type="email"
+                  placeholder="Email Address"
+                  className="rounded"
+                  value={values.email}
+                  onChange={handleChange}
+                  isValid={touched.email && !errors.email}
+                  isInvalid={!!errors.email}
+                />
+              </Form.Group>
+              <Form.Group as={Col} controlId="formGridPhone">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control
+                  name="phone"
+                  placeholder="+254 712..."
+                  className="rounded"
+                  value={values.phone}
+                  onChange={handleChange}
+                  isInvalid={!!errors.phone}
+                  isValid={touched.phone && !errors.phone}
+                />
+              </Form.Group>
+            </Form.Row>
+          </fieldset>
 
-      {/* Shipping Section */}
-      <fieldset>
-        <legend>Shipping Information</legend>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridFirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              name="firstName"
-              type="text"
-              placeholder="First Name"
-              className="rounded"
-            />
-          </Form.Group>
+          {/* Shipping Section */}
+          <fieldset>
+            <legend>Shipping Information</legend>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  name="firstName"
+                  type="text"
+                  placeholder="First Name"
+                  className="rounded"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  isInvalid={!!errors.firstName}
+                  isValid={touched.firstName && !errors.firstName}
+                />
+              </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridLastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              name="lastName"
-              type="text"
-              placeholder="Last Name"
-              className="rounded"
-            />
-          </Form.Group>
-        </Form.Row>
+              <Form.Group as={Col} controlId="formGridLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  name="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                  className="rounded"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  isInvalid={!!errors.lastName}
+                  isValid={touched.lastName && !errors.lastName}
+                />
+              </Form.Group>
+            </Form.Row>
 
-        <Form.Group controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            name="address"
-            placeholder="4th Ngong Avenue"
+            <Form.Group controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                name="address"
+                placeholder="4th Ngong Avenue"
+                className="rounded"
+                value={values.address}
+                onChange={handleChange}
+                isInvalid={!!errors.address}
+                isValid={touched.address && !errors.address}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formGridAddress2">
+              <Form.Label>Address 2 (optional)</Form.Label>
+              <Form.Control
+                name="address2"
+                placeholder="Apartment, studio, or floor"
+                className="rounded"
+                value={values.address2}
+                onChange={handleChange}
+                isInvalid={!!errors.address2}
+                isValid={touched.address2 && !errors.address2}
+              />
+            </Form.Group>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridTown">
+                <Form.Label>Town</Form.Label>
+                <Form.Control
+                  name="town"
+                  placeholder="Thika, Rongai, Naivasha ..."
+                  className="rounded"
+                  value={values.town}
+                  onChange={handleChange}
+                  isInvalid={!!errors.town}
+                  isValid={touched.town && !errors.town}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridCounty">
+                <Form.Label>County</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="county"
+                  value="Choose..."
+                  className="rounded"
+                  value={values.county}
+                  onChange={handleChange}
+                  isInvalid={!!errors.county}
+                  isValid={touched.county && !errors.county}
+                >
+                  <option value="">Choose...</option>
+                  <option value="Nairobi">Nairobi</option>
+                  <option value="Kajiado">Kajiado</option>
+                  <option value="Kiambu">Kiambu</option>
+                  <option value="Nakuru">Nakuru</option>
+                  <option value="Nyeri">Nyeri</option>
+                  <option value="Mombasa">Mombasa</option>
+                </Form.Control>
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group id="formGridCheckbox">
+              <Form.Check
+                name="terms"
+                type="checkbox"
+                label="Agree to terms and conditions"
+                value={values.terms}
+                onChange={handleChange}
+                isInvalid={!!errors.terms}
+                isValid={touched.terms && !errors.terms}
+              />
+            </Form.Group>
+          </fieldset>
+
+          <Button
+            variant="primary"
+            type="submit"
             className="rounded"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formGridAddress2">
-          <Form.Label>Address 2 (optional)</Form.Label>
-          <Form.Control
-            name="address2"
-            placeholder="Apartment, studio, or floor"
-            className="rounded"
-          />
-        </Form.Group>
-
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridTown">
-            <Form.Label>Town</Form.Label>
-            <Form.Control
-              name="town"
-              placeholder="Thika, Rongai, Naivasha ..."
-              className="rounded"
-            />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridCounty">
-            <Form.Label>County</Form.Label>
-            <Form.Control
-              as="select"
-              name="county"
-              value="Choose..."
-              className="rounded"
-            >
-              <option>Choose...</option>
-              <option>Nairobi</option>
-              <option>Kajiado</option>
-              <option>Kiambu</option>
-              <option>Nakuru</option>
-              <option>Nyeri</option>
-              <option>Mombasa</option>
-            </Form.Control>
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group id="formGridCheckbox">
-          <Form.Check
-            name="terms"
-            type="checkbox"
-            label="Agree to terms and conditions"
-          />
-        </Form.Group>
-      </fieldset>
-
-      <Button variant="primary" type="submit" className="rounded">
-        Continue to Payment
-      </Button>
-    </Form>
+            disabled= {isSubmitting || Object.keys(errors).length !== 0}
+          >
+            Continue to Payment
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
