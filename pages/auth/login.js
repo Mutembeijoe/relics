5105e64b2690496191f236971eccd86ff5c08b2e
mutembeijoe <mutembeijoe@gmail.com>
2020-06-04 12:30:48 +0300

    updates the login page with api changes

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -8,84 +8,132 @@ import { connect } from "react-redux";
 import { login } from "../../redux/user/actions";
 import _ from "lodash";
 import { useRouter } from "next/router";
-import cn from 'classnames'
-import styles from "../../styles/login_page.module.scss";
+import cn from "classnames";
+import Alert from "react-bootstrap/Alert";
+import styles from "../../styles/sign_up_page.module.scss";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { selectPreviousRoute } from "../../redux/route/selectors";
+import { deletePreviousRoute } from "../../redux/route/actions";
 
-const SignIn = ({ loginUser }) => {
+const SignIn = ({ loginUser, previousRoute, deletePreviousRoute }) => {
   const router = useRouter();
-
-  const [form, setInputs] = useState({
-    email: "",
-    password: "",
-  });
+  console.log(previousRoute);
 
   const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post("/api/users/login", {
-        email: form.email,
-        password: form.password,
-      });
-      const user = _.pick(response.data, ["username", "email", "token"]);
-      loginUser(user);
-      router.back();
-    } catch (err) {
-      const { error } = err.response.data;
-      setError(error);
-    }
-  };
+  const loginSchema = yup.object({
+    email: yup.string().email("Invalid email address").required(),
+    password: yup.string().required(),
+  });
 
   return (
     <Layout>
       <div className="container">
-        <div className={cn(styles.invalidResponse, {[styles.active]:error})}>{error}</div>
+        <Alert
+          variant="danger"
+          className={`${cn(styles.alert, {
+            [styles.active]: error,
+          })} mx-auto text-center`}
+        >
+          {error}
+        </Alert>
         <div className="row">
-          <div className="col-md-5 mx-auto" >
-            <Form onSubmit={handleSubmit}>
-              <fieldset>
-                <legend>Log In</legend>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    value={form.email}
-                    onChange={(e) =>
-                      setInputs({ ...form, email: e.target.value })
-                    }
-                    type="email"
-                    placeholder="Enter email"
-                    className="rounded"
-                  />
-                </Form.Group>
+          <div className="col-md-5 mx-auto">
+            <Formik
+              validationSchema={loginSchema}
+              initialValues={{ email: "", password: "" }}
+              onSubmit={async (value, actions) => {
+                try {
+                  const response = await axios.post("/api/users/login", {
+                    ...value,
+                  });
+                  const user = _.pick(response.data, [
+                    "username",
+                    "email",
+                    "token",
+                  ]);
+                  // save user to redux state
+                  loginUser(user);
 
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    value={form.password}
-                    onChange={(e) =>
-                      setInputs({ ...form, password: e.target.value })
-                    }
-                    type="password"
-                    placeholder="Password"
-                    className="rounded"
-                  />
-                </Form.Group>
-                <div className="d-flex justify-content-between">
-                  <Button variant="primary" type="submit" className="rounded">
-                    Log In
-                  </Button>
+                  actions.setSubmitting(false);
 
-                  <Form.Text className="text-muted">
-                    Don't Have an Account?
-                    <Link href="/auth/signup">
-                      <a className="font-weight-bold mx-2">Sign Up</a>
-                    </Link>
-                  </Form.Text>
-                </div>
-              </fieldset>
-            </Form>
+                  //redirect user accordingly
+
+                  if (previousRoute === "/auth/signup") {
+                    deletePreviousRoute()
+                    router.push("/");
+                  } else {
+                    router.back();
+                  }
+                } catch (error) {
+                  const { message } = error.response.data;
+                  setError(message);
+                }
+              }}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                values,
+                errors,
+                isSubmitting,
+              }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                  <fieldset>
+                    <legend>Log In</legend>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        type="email"
+                        placeholder="Enter email"
+                        className="rounded"
+                        isInvalid={!!errors.email}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.email}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        type="password"
+                        placeholder="Password"
+                        className="rounded"
+                        isInvalid={!!errors.password}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <div className="d-flex justify-content-between">
+                      <Button
+                        variant="primary"
+                        type="submit"
+                        className="rounded"
+                        disabled={isSubmitting}
+                      >
+                        Log In
+                      </Button>
+
+                      <Form.Text className="text-muted">
+                        Don't Have an Account?
+                        <Link href="/auth/signup">
+                          <a className="font-weight-bold mx-2">Sign Up</a>
+                        </Link>
+                      </Form.Text>
+                    </div>
+                  </fieldset>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
@@ -93,8 +141,13 @@ const SignIn = ({ loginUser }) => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  loginUser: (user) => dispatch(login(user)),
+const mapStateToProps = (state) => ({
+  previousRoute: selectPreviousRoute(state),
 });
 
-export default connect(null, mapDispatchToProps)(SignIn);
+const mapDispatchToProps = (dispatch) => ({
+  loginUser: (user) => dispatch(login(user)),
+  deletePreviousRoute: () => dispatch(deletePreviousRoute())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
