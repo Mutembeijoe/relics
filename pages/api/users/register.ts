0584268e05b837b98bdc7384e7de4439363d45ec
mutembeijoe <mutembeijoe@gmail.com>
2020-06-09 09:42:39 +0300

    installs next-connect lib for easier middleware handling :sparkles:

--- pages/api/users/register.ts
+++ pages/api/users/register.ts
@@ -5,60 +5,58 @@ import {
   verifyEmailExists,
 } from "../../../database/Queries/users/users";
 import { sendError } from "../../../utils/api_utils";
-import { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.method) {
-    case "POST":
-      const user = _.pick(req.body, ["username", "email", "password"]);
+const handler = nc();
 
-      // Verify that a user with sent email doesn't exist already
-      try {
-        const userExists = await verifyEmailExists(user.email);
-        if (userExists) {
-          sendError(res, {
-            status: 400,
-            message: "An account with the provided email already exists",
-            label: "email",
-          });
-          return;
-        }
-      } catch (error) {
-        sendError(res, {
-          status: 500,
-          message: "500 - Internal Server Error",
-        });
-        return;
-      }
+handler.post(async (req, res) => {
+  const user = _.pick(req.body, ["username", "email", "password"]);
 
-      // Generate salt and hash password
-      try {
-        const salt = await bcrypt.genSalt();
-        const hashed = await bcrypt.hash(user.password, salt);
-        user.password = hashed;
-      } catch (error) {
-        sendError(res, {
-          status: 500,
-          message: "500 - Internal Server Error",
-        });
-        return;
-      }
-
-      // Create and Save User
-      try {
-        const id = await createUser(user);
-        return res.status(200).json({
-          id,
-        });
-      } catch (error) {
-        sendError(res, {
-          status: 500,
-          message: "500 - Internal Server Error",
-        });
-        return;
-      }
-    default:
-      res.setHeader("Allow", ["POST"]);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+  // Verify that a user with sent email doesn't exist already
+  try {
+    const userExists = await verifyEmailExists(user.email);
+    if (userExists) {
+      sendError(res, {
+        status: 400,
+        message: "An account with the provided email already exists",
+        label: "email",
+      });
+      return;
+    }
+  } catch (error) {
+    sendError(res, {
+      status: 500,
+      message: "500 - Internal Server Error",
+    });
+    return;
   }
-};
+
+  // Generate salt and hash password
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(user.password, salt);
+    user.password = hashed;
+  } catch (error) {
+    sendError(res, {
+      status: 500,
+      message: "500 - Internal Server Error",
+    });
+    return;
+  }
+
+  // Create and Save User
+  try {
+    const id = await createUser(user);
+    return res.status(200).json({
+      id,
+    });
+  } catch (error) {
+    sendError(res, {
+      status: 500,
+      message: "500 - Internal Server Error",
+    });
+    return;
+  }
+});
+
+export default handler;
