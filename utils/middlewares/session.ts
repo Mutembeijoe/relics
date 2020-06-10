ad3a0a9aa22023a7e79af39ff2a2e9931a5d5947
mutembeijoe <mutembeijoe@gmail.com>
2020-06-10 13:27:46 +0300

    adds redis for session storage

--- utils/middlewares/session.ts
+++ utils/middlewares/session.ts
@@ -1,19 +1,27 @@
 import session, { SessionOptions } from "express-session";
+import redis, { RedisClient } from "redis";
+import { RedisStoreOptions, RedisStore } from "connect-redis";
+
+const Store: RedisStore = require("connect-redis")(session);
+const redisClient: RedisClient = redis.createClient();
+
+const redisStoreOptions: RedisStoreOptions = {
+  client: redisClient,
+};
 
 // expression sessions
 const sessionOptions: SessionOptions = {
-    secret: process.env.PRIVATE_KEY,
-    name: "sid",
-    cookie: {
-      maxAge: 1000 * 60 * 30,
-      secure: false,
-      sameSite: true,
-    },
-    rolling:true,
-    saveUninitialized:false,
-    resave:false
-  };
-  
-export default session({ ...sessionOptions })
+  store: new Store(redisStoreOptions),
+  secret: process.env.PRIVATE_KEY,
+  name: "sid",
+  cookie: {
+    maxAge: 1000 * 60 * 30,
+    secure: false,
+    sameSite: true,
+  },
+  rolling: true,
+  saveUninitialized: false,
+  resave: false,
+};
 
-
+export default session({ ...sessionOptions });
