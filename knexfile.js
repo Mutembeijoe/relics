8e44d37771dfec3829754b360be7a6302b59b6ed
mutembeijoe <mutembeijoe@gmail.com>
2020-06-12 08:45:33 +0300

    add knex production config

--- knexfile.js
+++ knexfile.js
@@ -21,4 +21,24 @@ module.exports = {
     },
     debug: true,
   },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_LOCAL_HOST,
+      user: process.env.DB_LOCAL_USER,
+      password: process.env.DB_LOCAL_PASSWORD,
+      database: process.env.DB_LOCAL_NAME,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    debug: false,
+  },
 };
